2b1b62ab37d545100f360669b4277926bda81fe7
Sawyer Shirley <sawyershirley@MacBook-Pro-107.local>
2018-11-26 12:36:41 -0600

    Initial Commit

---
+++ iOS Task Manager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Task Manager
+//
+//  Created by Sawyer Shirley on 11/26/18.
+//  Copyright © 2018 Sawyer Shirley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
